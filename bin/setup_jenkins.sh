f2f0892d15799de00cc9db9e1716b1cb58aa0028
vbalasub <vbalasub@redhat.com>
2019-07-30 13:48:11 -0500

    Updated Jenkins Pipeline name

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -44,9 +44,9 @@ oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # TBD
-oc new-build . --name ocp-tasks-pipeline --strategy=pipeline --context-dir=openshift-tasks -e GUID=${GUID}
+oc new-build . --name tasks-pipeline --strategy=pipeline --context-dir=openshift-tasks -e GUID=${GUID}
 
-oc set env bc/ocp-tasks-pipeline GUID=${GUID}
+oc set env bc/tasks-pipeline GUID=${GUID}
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
